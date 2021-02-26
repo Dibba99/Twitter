37ad1d6eb2f0e87a790169c2213439b8f86d15ff
Dibba Roy <dibba@Apollo-Macbook.home>
2021-02-26 18:55:37 -0500

    Added login auth with table view of time tweets

---
+++ Twitter/RoundButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundButton.swift
+//  Twitter
+//
+//  Created by Dibba Roy on 2/26/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 0{
+        didSet{
+        self.layer.cornerRadius = cornerRadius
+        }
+    }
+
+    @IBInspectable var borderWidth: CGFloat = 0{
+        didSet{
+            self.layer.borderWidth = borderWidth
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor = UIColor.clear{
+        didSet{
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
